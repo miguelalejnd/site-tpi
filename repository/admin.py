a7b615b07369b61b9df7647d51ccecbb9a09bd7b
Miguel Alejandro López Jiménez <lj16001@gmail.com>
2021-01-23 12:44:33 -0600

    Ordenar los recursos al ser mostrados en todas las intefaces

--- repository/admin.py
+++ repository/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
 from .models import RecursoImagen, RecursoSonido, RecursoEnlazado, Categoria
-# Register your models here.
 
 admin.site.register(RecursoImagen)
 admin.site.register(RecursoSonido)
